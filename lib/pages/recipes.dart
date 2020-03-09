97e4a3fa1f0c0d4c91db045df93688264592f78c
Radu <raducristian118@gmail.com>
2020-03-09 16:51:52 +0200

    remove unnecessary stuff

--- lib/pages/recipes.dart
+++ lib/pages/recipes.dart
@@ -1,13 +1,10 @@
-import 'package:compendium_mobile/services/recipes_service.dart';
 import 'package:flutter/material.dart';
 
 class RecipesPage extends StatelessWidget {
-  var appTitle="1";
   @override
   Widget build(BuildContext context) {
-    fetchAlbum().then((val) => appTitle = "Hello from recipes");
     return new Center(
-      child: new Text(appTitle),
+      child: new Text("Hello from recipes"),
     );
   }
 
